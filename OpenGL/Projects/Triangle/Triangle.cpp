c7a47b896c7494edbe32d9459b8cfd9bd058ec74
Egor Voronov <egorvoronovev@gmail.com>
2023-10-15 19:02:33 +0300

    feat(Triangle): draw triangle in OpenGL

---
+++ OpenGL/Projects/Triangle/Triangle.cpp
@@ -0,0 +1,90 @@
+#include <cstdio>
+#include <GL/glew.h>
+
+#include "GLFW/glfw3.h"
+
+
+
+GLFWwindow* window;
+
+int main()
+{
+    if (!glfwInit())
+    {
+        fprintf(stderr, "Failed to initialize GLFW\n");
+        getchar();
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    window = glfwCreateWindow(800, 800, "Triangle", NULL, NULL);
+    
+    if (window == NULL)
+    {
+        fprintf(stderr, "Failed to open GLFW window\n");
+        getchar();
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK)
+    {
+        fprintf(stderr, "Failed to initialize GLEW\n");
+        getchar();
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+    glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+
+    GLuint VertexArrayID;
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
+
+    static const GLfloat g_vertex_buffer_data[] = {
+        -1.0f,  -1.0f,  0.0f,
+         1.0f,  -1.0f,  0.0f,
+         0.0f,   1.0f,  0.0f
+    };
+
+    GLuint vertexbuffer;
+    glGenBuffers(1, &vertexbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+    do
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(
+            0,
+            3,
+            GL_FLOAT,
+            GL_FALSE,
+            0,
+            (void*)0
+            );
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glDisableVertexAttribArray(0);
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+    while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0);
+    
+    glfwTerminate();
+
+    return 0;
+}
