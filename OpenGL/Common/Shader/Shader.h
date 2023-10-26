30b22877cba0abef18d522388c0de8d8bc9df690
Egor Voronov <egorvoronovev@gmail.com>
2023-10-26 21:47:12 +0300

    OpenGL(Shader): new simple shader class

---
+++ OpenGL/Common/Shader/Shader.h
@@ -0,0 +1,18 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+
+
+class Shader
+{
+
+public:
+    
+    unsigned int ID;
+
+    Shader(const char* vertexPath, const char* fragmentPath);
+
+    void Use();
+};
+
+#endif
